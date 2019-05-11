c7096ee38ac7e68577a3dcfedc60549c9f5d508d
wndenis <winda.wzse@gmail.com>
2019-05-12 01:05:23 +0300

    Add sword item

---
+++ Assets/StopAnimationAtEnd.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopAnimationAtEnd : StateMachineBehaviour
+{
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (stateInfo.normalizedTime >= 1 || stateInfo.normalizedTime <= 0)
+        {
+            animator.speed = 0;
+        }
+    }
+}
---
+++ Assets/SwordUse.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class SwordUse : MonoBehaviour
+{
+    public Animator swordToAnimate;
+    private bool _opened;
+    private VRTK_InteractableObject _linkedObject;
+
+    protected virtual void OnEnable()
+    {
+        _linkedObject = _linkedObject == null ? GetComponent<VRTK_InteractableObject>() : _linkedObject;
+
+        if (_linkedObject != null)
+        {
+            _linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+            _linkedObject.InteractableObjectUnused += InteractableObjectUnused;
+        }
+    }
+
+    protected virtual void OnDisable()
+    {
+        if (_linkedObject != null)
+        {
+            _linkedObject.InteractableObjectUsed -= InteractableObjectUsed;
+            _linkedObject.InteractableObjectUnused -= InteractableObjectUnused;
+        }
+    }
+
+    protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+    {
+        _opened = true;
+        swordToAnimate.speed = 1;
+        swordToAnimate.SetFloat("speed", swordToAnimate.GetFloat("speed") * 1);
+    }
+
+    protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
+    {
+        _opened = false;
+        swordToAnimate.speed = 1;
+        swordToAnimate.SetFloat("speed", swordToAnimate.GetFloat("speed") * -1);
+    }
+}
+
