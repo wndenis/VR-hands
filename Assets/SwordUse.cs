0474d05088277b13a92cf219f415e17f59203d7f
wndenis <winda.wzse@gmail.com>
2019-05-16 04:38:30 +0300

    Steamvr migration

--- Assets/SwordUse.cs
+++ Assets/SwordUse.cs
@@ -1,55 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using VRTK;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
 
 public class SwordUse : MonoBehaviour
 {
     public Animator swordToAnimate;
     private bool _opened;
-    private VRTK_InteractableObject _linkedObject;
+    private SteamVR_Action_Boolean _pressed;
 
-    protected virtual void OnEnable()
+    private void Awake()
     {
-        _linkedObject = _linkedObject == null ? GetComponent<VRTK_InteractableObject>() : _linkedObject;
-
-        if (_linkedObject != null)
-        {
-            _linkedObject.InteractableObjectUsed += InteractableObjectUsed;
-            _linkedObject.InteractableObjectUnused += InteractableObjectUnused;
-        }
-        PlayAnimation(0);
+        _pressed[SteamVR_Input_Sources.Any].onStateDown += PlayAnimation;
     }
 
-    protected virtual void OnDisable()
+//
+//    protected virtual void OnEnable()
+//    {
+//        _linkedObject = _linkedObject == null ? GetComponent<VRTK_InteractableObject>() : _linkedObject;
+//
+//        if (_linkedObject != null)
+//        {
+//            _linkedObject.InteractableObjectUsed += InteractableObjectUsed;
+//            _linkedObject.InteractableObjectUnused += InteractableObjectUnused;
+//        }
+//        PlayAnimation(0);
+//    }
+//
+//    protected virtual void OnDisable()
+//    {
+//        if (_linkedObject != null)
+//        {
+//            _linkedObject.InteractableObjectUsed -= InteractableObjectUsed;
+//            _linkedObject.InteractableObjectUnused -= InteractableObjectUnused;
+//        }
+//    }
+//
+//    protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
+//    {
+//        _opened = true;
+//        PlayAnimation(1);
+//    }
+//
+//    protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
+//    {
+//        _opened = false;
+//        PlayAnimation(-1);
+//    }
+//    
+    private void PlayAnimation(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
     {
-        if (_linkedObject != null)
-        {
-            _linkedObject.InteractableObjectUsed -= InteractableObjectUsed;
-            _linkedObject.InteractableObjectUnused -= InteractableObjectUnused;
-        }
-    }
-
-    protected virtual void InteractableObjectUsed(object sender, InteractableObjectEventArgs e)
-    {
-        _opened = true;
-        PlayAnimation(1);
-    }
-
-    protected virtual void InteractableObjectUnused(object sender, InteractableObjectEventArgs e)
-    {
-        _opened = false;
-        PlayAnimation(-1);
-    }
-    
-    private void PlayAnimation(float speed)
-    {
+        _opened = !_opened;
+        var speed = _opened ? 1 : -1;
         var playTime = swordToAnimate.GetCurrentAnimatorStateInfo(0).normalizedTime;
         print(playTime);
         playTime = Mathf.Clamp01(playTime);
         swordToAnimate.SetFloat("direction", speed);
         swordToAnimate.Play("OpenClose", -1, playTime);
-        
     }
 }
 
